e41d48046ea59de910da26054d31e2ff6207aa7e
diskSpinner <fortkokos969@gmail.com>
2026-02-01 23:28:36 +0100

    co-authored by: david made calculator; learnt: basics

---
+++ advanced/calc.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void addition(float x, float y) {
+    float sum = x + y;
+    printf("Addition: %.2f\n", sum);
+}
+
+void subtraction(float x, float y) {
+    float sum = x - y;
+    printf("Subtraction: %.2f\n", sum);
+}
+
+void multiplication(float x, float y) {
+    float sum = x * y;
+    printf("Multiplication: %.2f\n", sum);
+}
+
+void division(float x, float y) {
+    float sum = x / y;
+    if (y == 0) {
+        printf("Invalid Input!\n");
+    }
+    else {
+        printf("Division: %.2f\n", sum);
+    }
+}
+
+void scan_input(float *x, float *y) {
+    printf("X: ");
+    scanf("%f", x);
+    printf("Y: ");
+    scanf("%f", y);
+}
+
+
+int main() {
+    while (true) {
+        float x;
+        float y;
+
+        int choice;
+    
+        printf("What do you want to do (1. Addition, 2. Subtraction, 3. Multiplication, 4. Division): ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            scan_input(&x, &y);
+            addition(x, y);
+        }
+        else if (choice == 2) {
+            scan_input(&x, &y);
+            subtraction(x, y);
+        }
+        else if (choice == 3) {
+            scan_input(&x, &y);
+            multiplication(x, y);
+        }
+        else if (choice == 4) {
+            scan_input(&x, &y);
+            division(x, y);
+        }
+    
+        else {
+            printf("Invalid choice!\n");
+        }
+    }
+}
