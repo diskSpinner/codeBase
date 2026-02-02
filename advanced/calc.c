0239102d3cd7724f63c3abfd1c855728370ca277
diskSpinner <fortkokos969@gmail.com>
2026-02-02 21:31:10 +0100

    more functions

--- advanced/calc.c
+++ advanced/calc.c
@@ -46,37 +46,64 @@ void scan_input(float *x, float *y) {
     scanf("%f", y);
 }
 
+void area_calculation_prc(float x, float y) {
+    int choice;
+    float x;
+    float y;
+    printf("What do you want to do (1. Square, 2. Rectangle)");
+    switch (choice) {
+        case 1:
+            scan_area_input_square(&x);
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+}
+
 int main() {
     while (true) {
         float x;
         float y;
         int choice;
     
-        printf("What do you want to do (1. Addition, 2. Subtraction, 3. Multiplication, 4. Division, 5. Area of Square): ");
+        printf("What do you want to do (1. Addition, 2. Subtraction, 3. Multiplication, 4. Division, 5. Area Calculation, 6. Advanced Functions ");
         scanf("%d", &choice);
 
-        if (choice == 1) {
+        switch (choice){
+        case 1:
             scan_input(&x, &y);
             addition(x, y);
-        }
-        else if (choice == 2) {
+            break;
+        case 2:
             scan_input(&x, &y);
             subtraction(x, y);
-        }
-        else if (choice == 3) {
+            break;
+        case 3:
             scan_input(&x, &y);
             multiplication(x, y);
-        }
-        else if (choice == 4) {
+            break;
+        case 4:
             scan_input(&x, &y);
             division(x, y);
-        }
-        else if (choice == 5) {
-            scan_area_input_square(&x);
-            area_of_square(x);
-        }
-        else {
+            break;
+        case 5:
+            // ...
+            break;
+        case 6:
+            area_calculation_prc(x,y);
+            break;
+        default:
             printf("Invalid choice!\n");
+            break;
         }
+        
     }
 }
+
+
+// switch cases in c (online) (done)
+// implement it into our calculator, make another "what type of area calculation" do you want in the 5. area calculations tab (in progress)
+// implement other area calculator functions like triangle, parallelogram, etc ( still in progress )
+// start first with square root calculation in the "advanced calculation" option (number 6. after area calculation)
---
+++ advanced/casetest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int choice = 1;
+
+    switch (choice) {
+        case 1:
+            printf("Hello!\n");
+            break;
+        case 2:
+            printf("Hey!\n");
+            break;
+        case 3:
+            printf("Bazinga!\n");
+            break;
+    }
+}
+// david are you alive?
+//
